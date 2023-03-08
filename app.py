d5de08996d4c1bcfc2e7e6cbce1420de231c6bb2
Ubuntu <ubuntu@ip-172-31-60-96.ec2.internal>
2023-03-08 19:34:30 +0000

    latest

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from sqlalchemy.sql import func
-
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://oreilly:hunter2@localhost"
-
-
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 class Books(db.Model):
-    work_id = db.Column(db.Integer)
-    tile = db.Column(db.String(100))
-    authors = db.Column(db.String(100))
-    isbn = db.Column(db.String(100))
+    __tablename__='works'
+
+    work_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    authors = db.Column(db.Text)
+    isbn = db.Column(db.Text)
     description = db.Column(db.Text)
+
+    def __init__(self, title, authors, isbn, description):
+        self.title = title
+        self.authors = authors
+        self.isbn = isbn
+        self.description = description
 
 @app.route('/')
 def index():
     books = Books.query.all()
-    return render_template('index.html', books=students)
+    return render_template('index.html', books=books)
