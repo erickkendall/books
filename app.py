68f9e1a060405c8b2882cf30c59cabe673c678c2
erickkendall <ekendallgithub@gmail.com>
2023-03-08 11:49:16 -0600

    latest

--- app.py
+++ app.py
@@ -4,28 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy.sql import func
 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://oreilly:hunter2@localhost"
+
+
+db = SQLAlchemy(app)
+
+class Books(db.Model):
+    work_id = db.Column(db.Integer)
+    tile = db.Column(db.String(100))
+    authors = db.Column(db.String(100))
+    isbn = db.Column(db.String(100))
+    description = db.Column(db.Text)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
-
-@app.route('/about/')
-def about():
-    return '<h3>This is a Flask web application.</h3>'
-
-@app.route('/capitalize/<word>')
-def capitalize(word):
-    return '<h1>{}</h1>'.format(escape(word.capitalize()))
-
-@app.route('/add/<int:n1>/<int:n2>/')
-def add(n1, n2):
-    return'<h1>{}</h1>'.format(n1 + n2)
-
-@app.route('/users/<int:user_id>/')
-def greet_user(user_id):
-    users = ['Bob', 'Jane', 'Adam']
-    try:
-        return '<h2>Hi {}</h2>'.format(users[user_id])
-    except IndexError:
-        abort(404)
+def index():
+    books = Books.query.all()
+    return render_template('index.html', books=students)
